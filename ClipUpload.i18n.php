7bbece27653b5d93d6e65d6ea81a17fef74d6f03
Bara Idrissa <bara.idris@gmail.com>
2020-07-09 17:16:59 +0200

    Adding italian language.

--- ClipUpload.i18n.php
+++ ClipUpload.i18n.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * Internationalisation file for extension ClipUpload.
- *  
+ *
  * @author SLboat
  *
  * @licence GNU General Public Licence 2.0 or later
  */
- 
+
 $messages = array();
- 
-/** English 
+
+/** English
  * @author SLboat
  * @author Kghbln
  */
@@ -59,7 +59,7 @@ $messages['de'] = array(
         'clipup-mwfeedbackerrorText' => '[Clip_Upload:Während des Hochladens ist ein Fehler aufgetreten (%s).]',
 );
 
-/** Chinese Simplified 
+/** Chinese Simplified
  * 中文简体，啊哈
  * @mod 森亮号大船
  */
@@ -74,4 +74,21 @@ $messages['zh-hans'] = array(
         'clipup-istoolarge' => ':[剪贴板上传: 剪贴板里的图片太大了-%sKB!]', #文件太大了
         'clipup-errdoingupload' => ':[剪贴板上传: 上传错误]', #上传的时候发生意外
         'clipup-mwfeedbackerrorText' => '[剪贴板上传: 服务器返回错误信息(%s)]',
+);
+
+/** Italian
+ * @author baraidris
+ */
+$messages['it'] = array(
+        'clipup-desc' => 'Permette di caricare file direttamente dagli appunti usando InlineAttachment',
+        'clipup-notLoadConfig' => '[Clip_Upload:La configurazione non è stata caricata]',
+        'clipup-progressText' => ':[Clip_Upload:Caricamento file …]()',
+        'clipup-failduploadText' => ':[Clip_Upload:Caricamento fallito (%s)]',
+        //please do not translate inside the flag%s. It will be replace some other text.
+        'clipup-uploadingText' => ':[Clip_Upload:Caricamento in corso %s (%s KB) …]',
+        'clipup-urlText' => ':[[File:%s]]',
+        'clipup-notsamesize' => ':[Clip_Upload:Il file non è identito]',
+        'clipup-istoolarge' => ':[Clip_Upload:Il file è troppo pesante: %s KB!]',
+        'clipup-errdoingupload' => ':[Clip_Upload:Si è verificato un errore durante il caricamento]',
+        'clipup-mwfeedbackerrorText' => '[Clip_Upload:Si è verificato un errore durante il caricamento (%s)]',
 );
